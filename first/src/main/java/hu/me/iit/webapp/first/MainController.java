d54013a64e9d3dcacec7d96b902a4ebca341ed7f
Gabszi2 <gabszi12345@gmail.com>
2021-09-28 10:25:15 +0200

    done

--- first/src/main/java/hu/me/iit/webapp/first/MainController.java
+++ first/src/main/java/hu/me/iit/webapp/first/MainController.java
@@ -1,19 +1,21 @@
+
 package hu.me.iit.webapp.first;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.awt.PageAttributes.MediaType;
 
 @Controller
 public class MainController {
-    @RequestMapping()
+
+
+    @GetMapping(path="/", produces= MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    ResponseDto gyoker (){
-        ResponseDto responseDto=new ResponseDto();
+    ResponseDto Gyoker() {
+        ResponseDto responseDto = new ResponseDto();
         responseDto.setId(123);
-        responseDto.setMessage("Hello");
+        responseDto.setMessage("hello");
         return responseDto;
     }
 }
